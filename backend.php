3ff701782d014ed7dd4c446638bc01861465e963
thelittlegiant18 <jesusjair18@gmail.com>
2022-10-25 17:07:52 -0500

    modified:   backend.php

--- backend.php
+++ backend.php
@@ -8,23 +8,104 @@ if (isset($_POST['conjunto1']) && isset($_POST['conjunto2'])) {
 
     $operacion = $_POST['operacion'];
 
-    if ($operacion === "interseccion") {
+    switch ($operacion) {
 
-        $conjunto1 = $_POST['conjunto1'];
-        $conjunto2 = $_POST['conjunto2'];
-        $formateoConjunto1 = str_replace([' ', '{', '}'], "", $conjunto1);
-        $test = explode(",", $formateoConjunto1);
-        $formateoConjunto2 = str_replace([' ', '{', '}'], "", $conjunto2);
-        $test2 = explode(",", $formateoConjunto2);
+        case 'union':
 
-        $interseccion = array_intersect($test, $test2);
+            $conjunto1 = $_POST['conjunto1'];
+            $conjunto2 = $_POST['conjunto2'];
+            $formateoConjunto1 = str_replace([' ', '{', '}'], "", $conjunto1);
+            $array1 = explode(",", $formateoConjunto1);
+            $formateoConjunto2 = str_replace([' ', '{', '}'], "", $conjunto2);
+            $array2 = explode(",", $formateoConjunto2);
 
-        echo "Conjunto 1 = {", $formateoConjunto1, "}";
-        echo "<br>";
-        echo "Conjunto 2 = {", $formateoConjunto2, "}";
-        echo "<br>";
-        echo "Conjunto 1 <b>∩</b> Conjunto 2 = ";
-        $arrayInterseccion = implode(', ', $interseccion);
-        echo "{", $arrayInterseccion, "}";
+            $union = array_unique(array_merge($array1, $array2));
+
+            echo "Conjunto 1 = {", $formateoConjunto1, "}";
+            echo "<br>";
+            echo "Conjunto 2 = {", $formateoConjunto2, "}";
+            echo "<br>";
+            echo "<b>Conjunto 1 ∪ Conjunto 2 = </b>";
+            $arrayUnion = implode(', ', $union);
+            echo "<b>{", $arrayUnion, "}</b>";
+
+            break;
+
+        case 'interseccion':
+
+            $conjunto1 = $_POST['conjunto1'];
+            $conjunto2 = $_POST['conjunto2'];
+            $formateoConjunto1 = str_replace([' ', '{', '}'], "", $conjunto1);
+            $array1 = explode(",", $formateoConjunto1);
+            $formateoConjunto2 = str_replace([' ', '{', '}'], "", $conjunto2);
+            $array2 = explode(",", $formateoConjunto2);
+
+            $interseccion = array_intersect($array1, $array2);
+
+            echo "Conjunto 1 = {", $formateoConjunto1, "}";
+            echo "<br>";
+            echo "Conjunto 2 = {", $formateoConjunto2, "}";
+            echo "<br>";
+            echo "<b>Conjunto 1 ∩ Conjunto 2 = </b>";
+            $arrayInterseccion = implode(', ', $interseccion);
+            echo "<b>{", $arrayInterseccion, "}</b>";
+
+            break;
+
+        case 'diferencia':
+
+            $conjunto1 = $_POST['conjunto1'];
+            $conjunto2 = $_POST['conjunto2'];
+            $formateoConjunto1 = str_replace([' ', '{', '}'], "", $conjunto1);
+            $array1 = explode(",", $formateoConjunto1);
+            $formateoConjunto2 = str_replace([' ', '{', '}'], "", $conjunto2);
+            $array2 = explode(",", $formateoConjunto2);
+
+            $diferencia = array_diff($array1, $array2);
+            $arrayDiferencia = implode(', ', $diferencia);
+
+            echo "Conjunto 1 = {", $formateoConjunto1, "}";
+            echo "<br>";
+            echo "Conjunto 2 = {", $formateoConjunto2, "}";
+            echo "<br>";
+            echo "<b>Conjunto 1 \ Conjunto 2 = </b>";
+            $arrayDiferencia = implode(', ', $diferencia);
+            echo "<b>{", $arrayDiferencia, "}</b>";
+
+            break;
+
+        case 'diferencia_simetrica':
+
+            $conjunto1 = $_POST['conjunto1'];
+            $conjunto2 = $_POST['conjunto2'];
+            $formateoConjunto1 = str_replace([' ', '{', '}'], "", $conjunto1);
+            $array1 = explode(",", $formateoConjunto1);
+            $formateoConjunto2 = str_replace([' ', '{', '}'], "", $conjunto2);
+            $array2 = explode(",", $formateoConjunto2);
+
+            $diferencia = array_diff($array1, $array2);
+            $arrayDiferencia = implode(', ', $diferencia);
+
+            $diferencia2 = array_diff($array2, $array1);
+
+            $diferenciaSimetrica = array_unique(array_merge($diferencia, $diferencia2));
+            $arrayDiferenciaSimetrica = implode(', ', $diferenciaSimetrica);
+
+            echo "Conjunto 1 = {", $formateoConjunto1, "}";
+            echo "<br>";
+            echo "Conjunto 2 = {", $formateoConjunto2, "}";
+            echo "<br>";
+            echo "<b>Conjunto 1 ∆ Conjunto 2 = </b>";
+            $arrayDiferenciaSimetrica = implode(', ', $diferenciaSimetrica);
+            echo "<b>{", $arrayDiferenciaSimetrica, "}</b>";
+
+            break;
+
+        default:
+
+            echo "<div class='alert alert-warning' role='alert'>
+                    No se ha podido resolver la operación, favor verificar
+            </div>";
+            break;
     }
 }
